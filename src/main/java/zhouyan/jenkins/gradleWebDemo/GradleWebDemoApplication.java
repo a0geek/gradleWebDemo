7c141c1ea19185fd926375489928c9264b406029
Yan Zhou <18001246829@163.com>
2019-05-19 17:36:19 +0800

    first commit

---
+++ src/main/java/zhouyan/jenkins/gradleWebDemo/GradleWebDemoApplication.java
@@ -0,0 +1,13 @@
+package zhouyan.jenkins.gradleWebDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleWebDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleWebDemoApplication.class, args);
+	}
+
+}
